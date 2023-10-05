ec7722fe34a9f5eac9fa60b31cde76c7c234ce17
Savio-Anjos <savioanjos2020@gmail.com>
2023-10-05 20:49:31 -0300

    :sparkles: feat: Route to list meals created

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,9 +1,29 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { knex } from "../database";
 import { randomUUID } from "crypto";
 
 export async function mealsRoutes(app: FastifyInstance) {
+  app.get("/", async (request: any, reply) => {
+    const user_id = request.query.user_id as string;
+
+    if (!user_id) {
+      return reply.status(400).send("'user_id' parameter is mandatory.");
+    }
+
+    const user = await knex("users").where("id", user_id).select();
+
+    if (user.length === 0) {
+      return reply.status(400).send("User does not exist!");
+    }
+
+    const meals = await knex("meals").where("user_id", user_id).select();
+
+    return {
+      meals,
+    };
+  });
+
   app.post("/", async (request, reply) => {
     const createMealsBodySchema = z.object({
       name: z.string(),
